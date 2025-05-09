864c151ed769b7247d17f76b665d3ac1024433e1
JonasArnold77 <105543936+JonasArnold77@users.noreply.github.com>
2025-05-09 13:07:08 +0200

    New Chunks

---
+++ Rock/Rock/Assets/Scripts/Obstacle/RandomChunk.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class RandomDictionary
+{
+    public ChallengeType challengeType;
+    public List<GameObject> value;
+}
+
+[Serializable]
+public class ObjectsByChallengeType
+{
+    public ChallengeType challengeType;
+    public List<GameObject> Objects;
+}
+
+public class RandomChunk : MonoBehaviour
+{
+    public List<ObjectsByChallengeType> ObjectByChallengeType = new List<ObjectsByChallengeType>();
+
+    private void Start()
+    {
+        StartCoroutine(Init());
+    }
+
+    public IEnumerator Init()
+    {
+        yield return new WaitUntil(() => LevelManager.Instance.GameIsInitialized);
+
+        foreach(var o in ObjectByChallengeType)
+        {
+            if (o.challengeType == ChallengeManager.Instance.actualChallengeButton.ActualChallengeType)
+            {
+                o.Objects.ForEach(o => o.SetActive(true));
+            }
+            else
+            {
+                o.Objects.ForEach(o => o.SetActive(false));
+            }
+        }
+    }
+}
